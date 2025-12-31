6ce34829f8a2d349a45c04cd3cf0b28ae0e436c3
Poprádi Árpád <arpad@popradiarpad.com>
2025-12-31 13:16:39 +0100

    Add background, Kermit log. Upgrade AGP.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -49,6 +49,7 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.kermit)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
